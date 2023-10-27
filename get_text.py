7ba090ae95210c73d3d151779d2d4ccbfe1f2e3b
ImSana <aasursyakov@gmail.com>
2023-10-27 16:13:19 +0300

    click on an element

--- get_text.py
+++ get_text.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
@@ -7,18 +8,16 @@ driver = webdriver.Chrome()
 driver.get("https://qa-mesto.praktikum-services.ru/")
 
 # Найди поле "Email" и заполни его
-...
-
+driver.find_element(By.ID, "email").send_keys('alexandr_sursyakov_2@yandex.ru')
 # Найди поле "Пароль" и заполни его
-...
-
+driver.find_element(By.ID, "password").send_keys('As89169565647')
 # Найди кнопку "Войти" и кликни по ней
-...
-
+driver.find_element(By.CLASS_NAME, "auth-form__button").click()
 # Добавь явное ожидание для загрузки страницы
-WebDriverWait(...).until(...)
+WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, "header__user")))
 
 # Найди кнопку, получи её текст и проверь, что он равен 'Выйти'
-assert ...
+
+assert driver.find_element(By.CLASS_NAME, "header__logout").text == 'Выйти'
 
 driver.quit()
